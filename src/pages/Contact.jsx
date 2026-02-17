ccd0ccf2d95a386b11d5a77183997a1ba5784182
JAYANTI THAKOR <jyantithakor941@gmail.com>
2026-02-17 11:34:41 +0530

    add copy selection using useRef

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,17 +1,27 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import {BsEyeSlash}   from "react-icons/bs";
 import { LiaEyeSolid } from "react-icons/lia";
 import './contact.css';
 const Contact = () => {
 
-  const [show,setShow] = useState(true)
+  const [show,setShow] = useState(true);
+
+  const inputPassword = useRef(null);
+
+  const copyPassword = async()=>{
+    inputPassword.current.focus();
+    inputPassword.current.select();
+    let password = inputPassword.current.value;
+    await navigator.clipboard.writeText(password);
+  }
 
   return (
     <div className='contact-form'>
         
-        <input type={show?"text":"password"}  placeholder='Enter password' />
+        <input type={show?"text":"password"} ref={inputPassword}  placeholder='Enter password' />
         <button className='btn' onClick={()=>setShow(!show)}>{!show? <BsEyeSlash />:<LiaEyeSolid />}</button>
-
+        <button onClick={copyPassword}>Copy Password</button>
+        <input type="text" placeholder='Paste password.!!' />
     </div>
   )
 }
